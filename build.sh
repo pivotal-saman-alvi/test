b05ffd4de7093f29a6415353c00daa8dbf80b90d
Saman A <salvi@pivotal.io>
2016-09-22 12:50:21 -0400

    git ocmmiting

--- build.sh
+++ build.sh
@@ -21,9 +21,12 @@ echo "hello" >> test.txt
 cd bosh-src
 git log -n 1 >> test2.txt
 
-echo "test" >> README.md
-# git commit -m "updating message"
+echo "test messageeeee" >> README.md
+
+git config --global user.email salvi@pivotal.io 
+git config --global user.name SamanGit
+
+git commit -m "updating message"
 
 cat test2.txt
-
 cat ../test.txt
