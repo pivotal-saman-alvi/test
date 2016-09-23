1a8535e4909df5dc2871f1c84a992bba124ea2b7
Saman A <salvi@pivotal.io>
2016-09-23 16:41:17 -0400

    modified build scripts

--- build.sh
+++ build.sh
@@ -13,26 +13,55 @@ set -e
 # bundle install --local
 # bundle exec rake --trace ci:publish_os_image_in_vm[$OPERATING_SYSTEM_NAME,$OPERATING_SYSTEM_VERSION,remote,$OS_IMAGE_S3_BUCKET_NAME,$OS_IMAGE_S3_KEY]
 
-echo "==================="
-echo $IMAGE_TO_BE_BUILT
-echo "==================="
+$OS_IMAGE
+$IMAGE_VERSION
 
+os_image_version="hashfrombuildthing"
 cd bosh-src
-# git log -n 1 >> test2.txt
 
+# get sha of commit that this os image was built for
+git log -n 1 >> sha.txt
+line=$(head -n 1 sha.txt)
+arr=($line)
+sha=${arr[1]}
+
+# grabbing message on commit
+message=`git log --pretty=oneline --abbrev-commit -n 1`
+echo -e $sha >> README.md
+echo -e $message >> README.md
+
+
+# replacing the right uploaded blob hash in the json file
+FILE=test.json
+
+while read line; do
+	if [[ $line =~ $OS_IMAGE && $line =~ $IMAGE_VERSION ]]
+	then
+		LAST_CHAR=""
+
+		# because we want to know whether or not we need a comma
+		if [[ ${line: -1} == "," ]]
+		then
+			LAST_CHAR=','
+		fi
+
+		line=${line/:*/:\"$os_image_version\"$LAST_CHAR}
+		echo $line
+	fi
+
+	echo $line >> tmprandomname.json
+done < $FILE
+
+cp tmprandomname.json $FILE
+rm -rf tmprandomname.json
+
+
+# committing
 git config --global user.email salvi@pivotal.io 
 git config --global user.name SamanGit
 
-echo -e "commit message 1\n" >> README.md
-
-git diff README.md >> diff.txt
-
 git add README.md
 git commit -m "updating message"
-
-echo "========================= diff"
-cat diff.txt
-echo "=========================="
 
 cd ..
 
